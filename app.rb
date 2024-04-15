b0f593a76d0cd396c97ea543b087d3246d13a3a0
yzhao24 <165528874+yzhao24@users.noreply.github.com>
2024-04-15 03:39:16 +0000

    divide is working

--- app.rb
+++ app.rb
@@ -31,10 +31,16 @@ get("/wizard_subtract") do
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num * @second_num
+
   erb(:mult_result)
 end
 
@@ -43,5 +49,10 @@ get("/divide") do
 end
 
 get("/wizard_divide") do
+
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num / @second_num  
   erb(:div_result)
 end
