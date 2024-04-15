4eaf9c9b7cebeda70ace946545ec6b30e14a4de7
yzhao24 <165528874+yzhao24@users.noreply.github.com>
2024-04-15 03:20:00 +0000

    add is working

--- app.rb
+++ app.rb
@@ -5,12 +5,13 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
   @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
 
   @result = @first_num + @second_num
   erb(:add_result)
